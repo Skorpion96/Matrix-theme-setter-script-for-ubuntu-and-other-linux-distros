c28dea6165e4f4a4b0e5761b91072b7ad4e37e62
Skorpion96 <39702358+Skorpion96@users.noreply.github.com>
2023-02-19 17:09:13 +0100

    Update matrix-setter-script.sh

--- matrix-setter-script.sh
+++ matrix-setter-script.sh
@@ -13,11 +13,11 @@ sudo cp -R "MATRIX.wmv" ~/.videos
 sudo apt install git -y
 git clone "https://github.com/ghostlexly/gpu-video-wallpaper.git"
 curl https://www.mediafire.com/file/y7ey8whbifjuaie/xwinwrap/file --output "xwinwrap"
-cp -R xwinwrap gpu-video-wallpaper
+sudo cp -R xwinwrap gpu-video-wallpaper
 cd gpu-video-wallpaper
 sudo ./install.sh
 cd ..
-cp -R MATRIX.wmv ~/.videos
+sudo cp -R MATRIX.wmv ~/.videos
 sudo sh video-wallpaper.sh --startup ~/.videos/MATRIX.wmv
 sudo echo 'export PS1="\e[0;32[\u@\h \W]\$ \e[m "' >> ~/.bashrc
 sudo gsettings set org.gnome.Terminal.Legacy.Profile:/org/gnome/terminal/legacy/profiles:/:"$(gsettings get org.gnome.Terminal.ProfilesList default | awk -F \' '{print $2}')"/ background-color '#000000'
